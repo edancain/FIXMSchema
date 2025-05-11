53464921973b69e706518a7ecc3cbcedf08995bd
Edan Cain <edan.cain@gmail.com>
2025-05-12 09:45:08 +1200

    Update dangerousgoods.go

--- generated/fixm/flight/cargo/dangerousgoods.go
+++ generated/fixm/flight/cargo/dangerousgoods.go
@@ -9,9 +9,9 @@ import (
 // The number referencing the air waybill.
 type AirWaybillNumberType base.CharacterStringType
 
-// Articles or substances which are capable of posing a risk to health, safety, ...
+// Articles or substances which are capable of posing a risk to health, safety, property or the environment and which are shown in the list of dangerous goods in the Technical Instructions or which are classified according to those Instructions. [ICAO Annex 18]
 type DangerousGoodsType struct {
-	// Describes whether the shipment is packed to comply with the limitations presc...
+	// Describes whether the shipment is packed to comply with the limitations prescribed for passenger and cargo aircraft or the limitations for cargo aircraft only.
 	AircraftLimitation *flight.AircraftDangerousGoodsLimitationType `xml:"aircraftLimitation"`
 	// The number referencing the air waybill.
 	AirWaybillNumber *flight.AirWaybillNumberType `xml:"airWaybillNumber"`
@@ -19,7 +19,7 @@ type DangerousGoodsType struct {
 	Extension []base.DangerousGoodsExtensionType `xml:"extension"`
 	// The location of a dangerous goods shipment inside the airframe.
 	OnboardLocation *base.CharacterStringType `xml:"onboardLocation"`
-	// A code that indicates the relative degree of danger presented by various arti...
+	// A code that indicates the relative degree of danger presented by various articles and substances within a Class or Division.
 	PackageGroup []flight.DangerousGoodsPackageGroupType `xml:"packageGroup"`
 	// IATA Shipper's Declaration for Dangerous Goods.
 	ShippingInformation *flight.ShippingInformationType `xml:"shippingInformation"`
@@ -29,9 +29,9 @@ type DangerousGoodsType struct {
 type ShippingInformationType struct {
 	// An extension hook for attaching extension (non-core) classes.
 	Extension []base.ShippingInformationExtensionType `xml:"extension"`
-	// Additional information related to an approval, permission, or other specific ...
+	// Additional information related to an approval, permission, or other specific detail regarding the shipment of dangerous goods.
 	ShipmentAuthorizations *base.CharacterStringType `xml:"shipmentAuthorizations"`
-	// An identifier of any subsidiary hazard class(es)/division(s) in addition to t...
+	// An identifier of any subsidiary hazard class(es)/division(s) in addition to the primary hazard class and division.
 	SubsidiaryHazardClassAndDivision *base.CharacterStringType `xml:"subsidiaryHazardClassAndDivision"`
 }
 
