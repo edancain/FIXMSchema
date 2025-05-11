23589bc332a8c9f706790cc1c7181bb5517792a6
Edan Cain <edan.cain@gmail.com>
2025-05-12 09:45:51 +1200

    Update radioactivematerials.go

--- generated/fixm/flight/cargo/radioactivematerials.go
+++ generated/fixm/flight/cargo/radioactivematerials.go
@@ -6,30 +6,33 @@ import (
 	"github.com/edancain/FIXMSchema.git/generated/fixm/base"
 )
 
-// The dimensionless number (rounded up to the next tenth) assigned to and place...
+// The dimensionless number (rounded up to the next tenth) assigned to and placed on the label of a fissile material package to designate the degree of control of accumulation of packages containing fissile material during transportation.
 type CriticalSafetyIndexType base.DecimalIndexType
 
-// A category used for radioactive materials in a package, overpack or freight c...
+// A category used for radioactive materials in a package, overpack or freight container, based on their maximum radiation level.
 type RadioactiveMaterialCategoryType string
 
 const (
+	// .Surface radiation &lt;0.5 millirem/hr, 1 meter radiation: N/A
 	RadioactiveMaterialCategoryTypeI_WHITE RadioactiveMaterialCategoryType = "I_WHITE"
+	// 
 	RadioactiveMaterialCategoryTypeII_YELLOW RadioactiveMaterialCategoryType = "II_YELLOW"
+	// Surface radiation &gt;50 millirem/hr, 1 meter radiation &gt;1 millirem/hr
 	RadioactiveMaterialCategoryTypeIII_YELLOW RadioactiveMaterialCategoryType = "III_YELLOW"
 )
 
-// A figure representing the radiation level measured at one meter from the pack...
+// A figure representing the radiation level measured at one meter from the package.
 type TransportIndexType base.DecimalIndexType
 
 // The grouping element for goods that contain radioactive materials.
 type RadioactiveMaterialType struct {
-	// A category used for radioactive materials in a package, overpack or freight c...
+	// >A category used for radioactive materials in a package, overpack or freight container, based on their maximum radiation level.
 	Category *flight.RadioactiveMaterialCategoryType `xml:"category"`
-	// The dimensionless number (rounded up to the next tenth) assigned to and place...
+	// The dimensionless number (rounded up to the next tenth) assigned to and placed on the label of a fissile material package to designate the degree of control of accumulation of packages containing fissile material during transportation.
 	CriticalSafetyIndex *flight.CriticalSafetyIndexType `xml:"criticalSafetyIndex"`
 	// An extension hook for attaching extension (non-core) classes.
 	Extension []base.RadioactiveMaterialExtensionType `xml:"extension"`
-	// A figure representing the radiation level measured at one meter from the pack...
+	// A figure representing the radiation level measured at one meter from the package.
 	TransportIndex *flight.TransportIndexType `xml:"transportIndex"`
 }
 
