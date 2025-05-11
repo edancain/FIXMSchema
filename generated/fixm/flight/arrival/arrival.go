8cd91e80c31fdcd6fd79541c401767b8d86e98bc
Edan Cain <edan.cain@gmail.com>
2025-05-12 09:26:57 +1200

    Update arrival.go

--- generated/fixm/flight/arrival/arrival.go
+++ generated/fixm/flight/arrival/arrival.go
@@ -10,12 +10,12 @@ import (
 type ArrivalTimeTypeType flight.TrajectoryPointPropertyTypeType
 
 const (
-	ArrivalTimeTypeTypeEND_LANDING_ROLL ArrivalTimeTypeType = "END_LANDING_ROLL"
-	ArrivalTimeTypeTypeIN_BLOCKS ArrivalTimeTypeType = "IN_BLOCKS"
-	ArrivalTimeTypeTypeWHEELS_ON ArrivalTimeTypeType = "WHEELS_ON"
+	ArrivalTimeTypeTypeEND_LANDING_ROLL ArrivalTimeTypeType = "END_LANDING_ROLL" // Indicates that the associated trajectory point corresponds to the point at which the aircraft is predicted to come to a full stop on the arrival runway. (A prediction only, the flight will likely exit the runway without coming to a full stop).
+	ArrivalTimeTypeTypeIN_BLOCKS ArrivalTimeTypeType = "IN_BLOCKS" // Indicates the point and time at which an arriving aircraft is/was in blocks.
+	ArrivalTimeTypeTypeWHEELS_ON ArrivalTimeTypeType = "WHEELS_ON" // Indicates that the associated trajectory point corresponds to the point at which the aircraft is predicted to be wheels on the runway for arrival.
 )
 
-// The time, type of time (wheels-on, in blocks, etc.), and geographical positio...
+// The time, type of time (wheels-on, in blocks, etc.), and geographical position of the arrival.  [FIXM]
 type ActualTimeOfArrivalType struct {
 	// An extension hook for attaching extension (non-core) classes.
 	Extension []base.ActualTimeOfArrivalExtensionType `xml:"extension"`
@@ -29,31 +29,34 @@ type ActualTimeOfArrivalType struct {
 
 // Information about the destination and actual arrival of the flight. [FIXM]
 type ArrivalType struct {
-	// The time, type of time (wheels-on, in blocks, etc.), and geographical positio...
+	// The time, type of time (wheels-on, in blocks, etc.), and geographical position of the arrival.  [FIXM]
 	ActualTimeOfArrival *flight.ActualTimeOfArrivalType `xml:"actualTimeOfArrival"`
 	// The identifier of the arrival airport slot allocated to the flight. [FIXM]
 	AirportSlotIdentification *base.AirportSlotIdentificationType `xml:"airportSlotIdentification"`
 	// The aerodrome at which the flight has actually arrived. [FIXM]
 	ArrivalAerodrome *base.AerodromeReferenceType `xml:"arrivalAerodrome"`
-	// The aerodrome of intended landing. [ICAO Doc 4444 - extracted from the defini...
+	// The aerodrome of intended landing. [ICAO Doc 4444 - extracted from the definition of Destination Alternate]
+	// This is the aerodrome at which the flight is scheduled to arrive. [FIXM]
 	DestinationAerodrome *base.AerodromeReferenceType `xml:"destinationAerodrome"`
-	// An alternate aerodrome to which an aircraft may proceed should it become eith...
+	// An alternate aerodrome to which an aircraft may proceed should it become either impossible or inadvisable to land at the aerodrome of intended landing. [ICAO Doc 4444]
 	DestinationAerodromeAlternate []base.AerodromeReferenceType `xml:"destinationAerodromeAlternate"`
 	// Specifies the previous destination aerodrome value when a change is made.
 	DestinationAerodromePrevious *base.AerodromeReferenceType `xml:"destinationAerodromePrevious"`
 	// An extension hook for attaching extension (non-core) classes.
 	Extension []base.ArrivalExtensionType `xml:"extension"`
-	// The route details to the revised destination aerodrome, followed by the ICAO ...
+	// The route details to the revised destination aerodrome, followed by the ICAO four-letter location indicator of
+	// the aerodrome. The revised route is subject to reclearance in flight. [ICAO Doc 4444]
 	ReclearanceInFlight *flight.ReclearanceInFlightType `xml:"reclearanceInFlight"`
 	// The runway direction for landing. [FIXM]
 	RunwayDirection *base.RunwayDirectionDesignatorType `xml:"runwayDirection"`
 }
 
-// The route details to the revised destination aerodrome, followed by the ICAO ...
+// The route details to the revised destination aerodrome, followed by the ICAO four-letter location indicator of
+// the aerodrome. The revised route is subject to reclearance in flight. [ICAO Doc 4444]
 type ReclearanceInFlightType struct {
 	// An extension hook for attaching extension (non-core) classes.
 	Extension []base.ReclearanceInFlightExtensionType `xml:"extension"`
-	// Aerodrome where the flight will divert to based on the revised route. For exa...
+	// Aerodrome where the flight will divert to based on the revised route. For example, an aerodrome where the flight will divert to if more fuel is used than expected.[FIXM]
 	FiledRevisedDestinationAerodrome *base.AerodromeReferenceType `xml:"filedRevisedDestinationAerodrome"`
 	// Route to the revised destination aerodrome. [FIXM]
 	RouteToRevisedDestination *base.CharacterStringType `xml:"routeToRevisedDestination"`
